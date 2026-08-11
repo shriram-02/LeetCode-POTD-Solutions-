6b654030d5e16d517f7161b1d1a7f96d219ad09d
Shriram Lahane <172600005+shriram-02@users.noreply.github.com>
2026-08-11 08:12:31 +0530

    Add files via upload

---
+++ LeetCode-POTD-Solutions/Smallest Missing Integer Greater Than Sequential Prefix Sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int missingInteger(int[] nums) {
+        int sum = nums[0];
+
+        int i = 1;
+        while (i < nums.length && nums[i] == nums[i - 1] + 1) {
+            sum += nums[i];
+            i++;
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        while (set.contains(sum)) {
+            sum++;
+        }
+
+        return sum;
+    }
+}
