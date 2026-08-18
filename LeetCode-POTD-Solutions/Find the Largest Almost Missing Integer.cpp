50ee097e49d8efe916585a661bdddad06814d25b
Shriram Lahane <172600005+shriram-02@users.noreply.github.com>
2026-08-18 11:25:09 +0530

    Add files via upload

---
+++ LeetCode-POTD-Solutions/Find the Largest Almost Missing Integer.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> count(51, 0);
+
+        for (int i = 0; i <= n - k; i++) {
+            vector<bool> seen(51, false);
+
+            for (int j = i; j < i + k; j++) {
+                if (!seen[nums[j]]) {
+                    count[nums[j]]++;
+                    seen[nums[j]] = true;
+                }
+            }
+        }
+
+        for (int x = 50; x >= 0; x--) {
+            if (count[x] == 1)
+                return x;
+        }
+
+        return -1;
+    }
+};
