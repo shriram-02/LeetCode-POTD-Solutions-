05e95d8470a9f73821ea10daa96c88b76d902625
Shriram Lahane <172600005+shriram-02@users.noreply.github.com>
2026-09-09 09:15:46 +0530

    Add files via upload

---
+++ LeetCode-POTD-Solutions/Count Commas in Range II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+        
+        for (long long p = 1000; p <= n; p *= 1000) {
+            ans += n - p + 1;
+            if (p > n / 1000) break;
+        }
+        
+        return ans;
+    }
+};
