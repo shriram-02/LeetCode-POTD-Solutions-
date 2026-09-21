29544899ad3e9abd5014e82c859d0b570bf919bb
Shriram Lahane <172600005+shriram-02@users.noreply.github.com>
2026-09-21 08:41:25 +0530

    Add files via upload

---
+++ LeetCode-POTD-Solutions/Find X Value of Array I.java
@@ -0,0 +1,27 @@
+class Solution {
+    public long[] resultArray(int[] nums, int k) {
+        long[] result = new long[k];
+        long[] dp = new long[k];
+
+        for (int num : nums) {
+            long[] next = new long[k];
+
+            int val = num % k;
+            next[val]++;
+
+            for (int r = 0; r < k; r++) {
+                if (dp[r] > 0) {
+                    next[(int)((long) r * val % k)] += dp[r];
+                }
+            }
+
+            for (int r = 0; r < k; r++) {
+                result[r] += next[r];
+            }
+
+            dp = next;
+        }
+
+        return result;
+    }
+}
